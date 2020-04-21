bf137c783dcf5bd79bd044bd114179b5f4f041fd
Mary Waithera <njihiamary11@gmail.com>
2020-04-21 17:57:04 +0300

    Add index and movie css files and link them in their respective templates using url_for

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from .config import DevConfig
 
-#Intialize application, creat application instance
-app = Flask(__name__)
+#Initialize application, creat application instance
+#We pass in instance_relative_config which allow us to connect to the instance/folder when the app instance is created
+app = Flask(__name__,instance_relative_config=True)
 
 # Setting up configuration
+#The app.config.from_pyfile('config.py') connects to the config.py file and all its contents are appended to the app.config.
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
+
 
 from app import views
 
+#most flask extensions have to be initialized in this file before they can be used
+from flask_bootstrap import Bootstrap
+
+#Initialize boostrap
+bootstrap = Bootstrap(app)
