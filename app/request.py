d9311bff71b12576656e4fa59115de41a9be3218
Mary Waithera <njihiamary11@gmail.com>
2020-04-22 12:28:23 +0300

    Create reviews file

--- app/request.py
+++ app/request.py
@@ -81,3 +81,25 @@ def get_movie(id):
             movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
 
     return movie_object 
+
+def search_movie(movie_name):
+
+    '''
+    Function gets and returns the movie that a user is searching for
+    '''
+
+    search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
+    with urllib.request.urlopen(search_movie_url) as url:
+        search_movie_data = url.read()
+        #convert received data into python dictionary
+        search_movie_response = json.loads(search_movie_data)
+
+        search_movie_results = None
+
+        if search_movie_response['results']:
+            search_movie_list = search_movie_response['results']
+            search_movie_results = process_results(search_movie_list)
+
+    return search_movie_results
+
+
