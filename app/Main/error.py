e30a803563be5018304a00d23bdd5e51185caa99
Mary Waithera <njihiamary11@gmail.com>
2020-04-23 12:15:45 +0300

    Import and define the main instance as the decorator; add app_errorhandler to ensure appliciation-wide access

--- app/Main/error.py
+++ app/Main/error.py
@@ -1,8 +1,8 @@
 from flask import render_template
-from app import app
+from . import main
 
 # decorator app.errorhandler() passes in the received error
-@app.errorhandler(404)
+@main.app_errorhandler(404)
 def four_Ow_four(error):
     '''
     method to render the 404 error page if the wrong address is entered
